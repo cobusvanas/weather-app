57cdae57909157dbb46a97d25f856d25d785bb0a
Cobus van As <cobusvanas@gmail.com>
2025-04-30 23:37:31 +0200

    UI changes

--- src/components/ErrorBlock.tsx
+++ src/components/ErrorBlock.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { VStack, Text, Button } from '@chakra-ui/react';
+import { VStack, Text, Button, Box } from '@chakra-ui/react';
 
 interface ErrorBlockProps {
   message: string;
@@ -11,17 +11,32 @@ const ErrorBlock: React.FC<ErrorBlockProps> = ({ message }) => {
   };
 
   return (
-    <VStack align="center" justify="center" height="100vh">
-      <Text fontSize="xl" color="red.500" fontWeight="bold">
-        Oops! Something went wrong.
-      </Text>
-      <Text fontSize="md" color="gray.600" textAlign="center">
-        {message}
-      </Text>
-      <Button variant="surface" onClick={handleReload}>
-        Refresh Page
-      </Button>
-    </VStack>
+    <Box
+      width="50%"
+      padding="6"
+      borderWidth="1px"
+      borderRadius="lg"
+      boxShadow="lg"
+      backgroundColor="white"
+      margin="auto"
+    >
+      <VStack align="center" justify="center" height="30vh">
+        <Text fontSize="xl" color="red.500" fontWeight="bold">
+          Oops! Something went wrong.
+        </Text>
+        <Text fontSize="md" color="gray.600" textAlign="center">
+          {message}
+        </Text>
+        <Button
+          variant="surface"
+          color={'blue.500'}
+          backgroundColor={'blue.50'}
+          onClick={handleReload}
+        >
+          Refresh Page
+        </Button>
+      </VStack>
+    </Box>
   );
 };
 
--- src/components/LoadingSpinner.tsx
+++ src/components/LoadingSpinner.tsx
@@ -1,14 +1,24 @@
 import React from 'react';
-import { VStack, Spinner, Text } from '@chakra-ui/react';
+import { VStack, Spinner, Text, Box } from '@chakra-ui/react';
 
 const LoadingSpinner: React.FC = () => {
   return (
-    <VStack align="center" justify="center" height="100vh">
-      <Spinner size="xl" color="blue.500" />
-      <Text fontSize="lg" color="gray.600">
-        Please wait while we fetch weather data...
-      </Text>
-    </VStack>
+    <Box
+      width="50%"
+      padding="6"
+      borderWidth="1px"
+      borderRadius="lg"
+      boxShadow="lg"
+      backgroundColor="white"
+      margin="auto"
+    >
+      <VStack align="center" justify="center" height="30vh">
+        <Spinner size="xl" color="blue.500" />
+        <Text fontSize="lg" color="gray.600" textAlign="center" marginTop={4}>
+          Please wait while we fetch weather data...
+        </Text>
+      </VStack>
+    </Box>
   );
 };
 
